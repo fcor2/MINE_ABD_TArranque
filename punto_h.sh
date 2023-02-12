598608b6fb4b289379e92a3b9cef94bbd036b706
fco_r2 <fcorrearozo@gmail.com>
2023-02-12 14:28:48 -0500

    Script para punto h. Usar script > archivo_out[.txt] | at [ver man]

---
+++ punto_h.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Corre primer archivo
+python d-n_palabras_mas_freq.py --archivo=reut2-000.sgm --n=10
+
+#Corre segundo archivo
+python d-n_palabras_mas_freq.py --archivo=reut2-001.sgm --n=10
+
+#Corre tercer archivo
+python d-n_palabras_mas_freq.py --archivo=reut2-002.sgm --n=10
+
